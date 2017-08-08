3f475246c455490f3d00e80f2a8cdcb2e2d21d65
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2017-08-07 23:51:08 -0700

    rework to ReST

--- update-docs.sh
+++ update-docs.sh
@@ -4,3 +4,5 @@ export PYTHONIOENCODING=utf8
 PYTHONPATH=./ scripts/build_readme.py > README.md && \
 PYTHONPATH=./ scripts/build_docs.sh && \
 PYTHONPATH=./ scripts/fix_links.py docs/*
+pandoc --columns=100 --output=README.rst --to=rst README.md
+rm README.md
