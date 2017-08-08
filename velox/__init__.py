5ff50b07f59dbfa1a8b69a6c949d423ce2cdacf2
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2017-08-08 11:50:01 -0700

    update readme docs

--- velox/__init__.py
+++ velox/__init__.py
@@ -9,6 +9,7 @@ u"""
 
 [![Build Status](https://travis-ci.org/lukedeo/Velox.svg?branch=master)](https://travis-ci.org/lukedeo/Velox)
 [![Coverage Status](https://coveralls.io/repos/github/lukedeo/Velox/badge.svg?branch=master)](https://coveralls.io/github/lukedeo/Velox?branch=master)
+[![PyPI version](https://badge.fury.io/py/Velox.svg)](https://badge.fury.io/py/Velox)
 
 Deploying and managing live machine learning models is difficult. It involves a mix of handling model versioning, hot-swapping new versions and determining version constraint satisfaction on-the-fly, and managing binary file movement either on a networked or local file system or with a cloud storage system like S3. Velox can handle this for you with a simple base class enforcing opinionated methods of handling the above problems.
 
@@ -20,13 +21,17 @@ Velox provides two main utilities:
 ## Requirements
 ---
 
-Velox currently only supports Python 2.7, but **we would love contributions towards Python 3 support** 😁
+~~Velox currently only supports Python 2.7, but **we would love contributions towards Python 3 support** 😁~~
+
+**Velox supports Python 2 and Python 3**
 
 The main requirements are `apscheduler` for scheduling hot-swaps, `semantic_version` for version sanity, and the `futures` Python 2.7 backport. If you want to be able to work with S3, you'll need `boto3` (and a valid and properly set up AWS account).
 
 To run the tests, you'll need the brilliant `moto` library, the `backports.tempfile` library for Python 2.7 compatibility, and `Keras` and `sckit-learn`.
 
 For logging, simply grab the Velox logger by the `velox` handle.
+
+You can install Velox using `pip install velox`, 
 
 ## `VeloxObject` Abstract Base Class
 ---
@@ -79,6 +84,8 @@ Here is a full example using [`gensim`](https://github.com/RaRe-Technologies/gen
     from gensim.corpora import Dictionary
     from gensim.models.ldamulticore import LdaMulticore
     from spacy.en import English
+
+    from velox import VeloxObject, register_model
 
 
     nlp = English()
