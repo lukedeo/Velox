c9758ce78ea8954da6b55c211dc2707f630ca15a
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2017-10-22 11:44:44 -0700

    new PyPI release

--- velox/__init__.py
+++ velox/__init__.py
@@ -232,7 +232,7 @@ import logging
 # supress "No handlers could be found for logger XXXXX" error
 logging.getLogger('velox').addHandler(logging.NullHandler())
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 
 from .obj import VeloxObject, register_model
 
